4d52f095e2781c28a126a5fdfd2a0ec70b85839a
hung794 <quochung71294@gmail.com>
2022-05-30 21:02:07 +0700

    1.4-Lap-Template-User

--- EatClean/Controllers/HomeController.cs
+++ EatClean/Controllers/HomeController.cs
@@ -13,17 +13,28 @@ namespace EatClean.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Repice()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
             return View();
         }
     }
