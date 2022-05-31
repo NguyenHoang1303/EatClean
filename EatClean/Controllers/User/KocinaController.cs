fe6e94b54301c07b8212481a071f41c0d8b28b71
Trinh Huy <huytnth2010022@fpt.edu.vn>
2022-05-31 11:57:57 +0700

    31/5

---
+++ EatClean/Controllers/User/KocinaController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EatClean.Controllers.User
+{
+    public class KocinaController : Controller
+    {
+        // GET: Kocina
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Recipe()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Articles()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
