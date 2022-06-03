e414edd867598cb78f1b2a62396a09c676f843a5
hung794 <quochung71294@gmail.com>
2022-06-03 17:14:13 +0700

    show-articles

--- EatClean/App_Start/IdentityConfig.cs
+++ EatClean/App_Start/IdentityConfig.cs
@@ -2,11 +2,8 @@
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
+[assembly: OwinStartup(typeof(EatClean.App_Start.IdentityConfig))]
 namespace EatClean.App_Start
 {
     public class IdentityConfig
