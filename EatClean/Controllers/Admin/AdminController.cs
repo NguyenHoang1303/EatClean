b88a58593dca923c35041a7c0cbd9701579e296a
hung794 <quochung71294@gmail.com>
2022-06-07 15:19:29 +0700

    Admin-Tag

--- EatClean/Controllers/Admin/AdminController.cs
+++ EatClean/Controllers/Admin/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using EatClean.Data;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,11 @@ namespace EatClean.Entity
             this._db = new DataContext();
         }
         // GET: Admin
+        public ActionResult Tag()
+        {
+            var tags = _db.Tags.ToList();
+            return View("~/Views/Admin/Tag/Index.cshtml", tags);
+        }
 
         public ActionResult CreateTag()
         {
@@ -49,6 +55,46 @@ namespace EatClean.Entity
             }
 
             return "False";
+        }
+
+        public ActionResult EditTag(int? id)
+        {
+            var tag = _db.Tags.Find(id);
+            return View("~/Views/Admin/Tag/Edit.cshtml", tag);
+        }
+
+        [HttpPost]
+        public bool DeleteTag(int? id)
+        {
+            var tag = _db.Tags.Find(id);
+            if(tag == null)
+            {
+                return false;
+            }
+            else
+            {
+                _db.Tags.Remove(tag);
+                _db.SaveChanges();
+                return true;
+            }
+        }
+
+        [HttpPost]
+        public bool EditTag(Tag tag)
+        {
+            var result = _db.Tags.Find(tag.Id);
+            if(result == null)
+            {
+                return false;
+            }
+            else
+            {
+                result.Name = tag.Name;
+                result.UpdatedAt = DateTime.Now.Ticks;
+                _db.Tags.AddOrUpdate(result);
+                _db.SaveChanges();
+                return true;
+            }
         }
     }
 }
--- EatClean/Migrations/Configuration.cs
+++ EatClean/Migrations/Configuration.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Migrations;
-using System.Linq;
-using EatClean.Data;
-using EatClean.Entity;
-using Newtonsoft.Json;
-
-namespace EatClean.Migrations
+﻿namespace EatClean.Migrations
 {
+    using EatClean.Entity;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
     internal sealed class Configuration : DbMigrationsConfiguration<EatClean.Data.DataContext>
     {
         public Configuration()
@@ -18,47 +15,21 @@ namespace EatClean.Migrations
 
         protected override void Seed(EatClean.Data.DataContext context)
         {
-           
-            // var articles = new List<Article>();
-            // for (int i = 0; i < 10; i++)
-            // {
-            //     var a = new Article()
-            //     {
-            //         Id = i + 1,
-            //         AuthorId = 1,
-            //         CategoryId = 1,
-            //         Title = Faker.Lorem.Sentence(),
-            //         Description = Faker.Lorem.Sentence(),
-            //         Thumbnail = Faker.Lorem.Paragraph(),
-            //         Status = 1,
-            //         CreatedAt = DateTime.Now.Ticks,
-            //         UpdatedAt = DateTime.Now.Ticks,
-            //         DeletedAt = DateTime.Now.Ticks 
-            //     };
-            //     articles.Add(a);
-            // }
-            //
-            // var articleDetails = new List<ArticleDetail>();
-            // for (int i = 0; i < 10; i++)
-            // {
-            //     
-            //     var a = new ArticleDetail
-            //     {
-            //         Id = 1,
-            //         Content = seedJson(),
-            //         Status = 1,
-            //         CreatedAt = DateTime.Now.Ticks,
-            //         UpdatedAt = DateTime.Now.Ticks,
-            //         DeletedAt = DateTime.Now.Ticks
-            //     };
-            //     articleDetails.Add(a);
-            // }
-            //
-            // var dataContext = new DataContext();
-            // articles.ForEach(a=> dataContext.Articles.AddOrUpdate(a));
-            // dataContext.SaveChanges();
-            // articleDetails.ForEach(a => dataContext.ArticleDetails.AddOrUpdate(a));
-            // dataContext.SaveChanges();
+            var adminRole = new Role()
+            {
+                Name = "Admin",
+                CreatedAt = DateTime.Now.Ticks,
+                UpdatedAt = DateTime.Now.Ticks
+            };
+            var userRole = new Role()
+            {
+                Name = "User",
+                CreatedAt = DateTime.Now.Ticks,
+                UpdatedAt = DateTime.Now.Ticks
+            };
+            context.Roles.Add(adminRole);
+            context.Roles.Add(userRole);
+            context.SaveChanges();
         }
     }
 }
