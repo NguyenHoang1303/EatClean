7f635981839e3065089d7d4d531cee749f8faea6
Trinh Ngoc Huy <huytnth2010022@fpt.edu.vn>
2022-06-07 18:19:37 +0700

    171612

---
+++ EatClean/Entity/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EatClean.Entity
+{
+    public class Book
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Thumbnail { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
---
+++ EatClean/Migrations/202206071111272_create_book_table.cs
@@ -0,0 +1,31 @@
+﻿namespace EatClean.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class create_book_table : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Price = c.Double(nullable: false),
+                        Thumbnail = c.String(),
+                        AuthorName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AlterColumn("dbo.Articles", "AuthorId", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Articles", "AuthorId", c => c.String());
+            DropTable("dbo.Books");
+        }
+    }
+}
