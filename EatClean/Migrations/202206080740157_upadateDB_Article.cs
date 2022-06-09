a263f4ba2bf329906742d56c8c17b22f46d3ff4c
nguyenhs <nguyen96hy@gmail.com>
2022-06-09 09:53:55 +0700

    upadte 6/9

---
+++ EatClean/Migrations/202206080740157_upadateDB_Article.cs
@@ -0,0 +1,22 @@
+﻿namespace EatClean.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class upadateDB_Article : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Articles", "Account_Id", c => c.String(maxLength: 128));
+            CreateIndex("dbo.Articles", "Account_Id");
+            AddForeignKey("dbo.Articles", "Account_Id", "dbo.AspNetUsers", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Articles", "Account_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.Articles", new[] { "Account_Id" });
+            DropColumn("dbo.Articles", "Account_Id");
+        }
+    }
+}
